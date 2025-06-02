c03e7fb30b609339edc7bd725141c0dc5b27a116
Saurabh Bisht <saurabhbisht076@gmail.com>
2025-06-02 15:25:39 +0530

    fix: update hospital schema validation

---
+++ backend/test.js
@@ -0,0 +1,4 @@
+// import express from 'express';
+// import { createServer } from 'http';        
+// import { Server } from 'socket.io';
+// import { createAdapter } from '@socket.io/redis-adapter'
